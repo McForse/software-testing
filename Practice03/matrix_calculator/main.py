05213a851ec821fd2ff2882714708bc01a972aad
Danil Vorozheykin <danilvda@gmail.com>
2021-10-03 16:08:09 +0300

    Add logic to matrix to pass exception test for Practice 3

--- Practice03/matrix_calculator/main.py
+++ Practice03/matrix_calculator/main.py
@@ -20,17 +20,28 @@ class Matrix:
         col = len(self.__matrix[0])
         for i in range(1, self.__row):
             if col != len(self.__matrix[i]):
-                raise RuntimeError
+                raise RuntimeError('Некорректная инициализация матрицы')
         return col
 
+    def __dimension_equals(self, other):
+        if self.__row == other.get_row_count() and self.__col == other.get_col_count():
+            return True
+        return False
+
     def sum(self, other):
-        return [[self.__matrix[i][j] + other.get_matrix()[i][j] for j in range(self.__col)] for i in range(self.__row)]
+        if self.__dimension_equals(other):
+            return [[self.__matrix[i][j] + other.get_matrix()[i][j] for j in range(self.__col)] for i in
+                    range(self.__row)]
+        raise RuntimeError('Разная размерность матриц')
 
     def __add__(self, other):
         return self.sum(other)
 
     def sub(self, other):
-        return [[self.__matrix[i][j] - other.get_matrix()[i][j] for j in range(self.__col)] for i in range(self.__row)]
+        if self.__dimension_equals(other):
+            return [[self.__matrix[i][j] - other.get_matrix()[i][j] for j in range(self.__col)] for i in
+                    range(self.__row)]
+        raise RuntimeError('Разная размерность матриц')
 
     def __sub__(self, other):
         return self.sub(other)
@@ -39,8 +50,11 @@ class Matrix:
         return [[i * number for i in self.__matrix[_row]] for _row in range(self.__row)]
 
     def mul(self, other):
-        return [[sum([self.__matrix[i][k] * other.get_matrix()[k][j] for k in range(other.get_row_count())]) for j in
+        if self.__col == other.get_row_count():
+            return [
+                [sum([self.__matrix[i][k] * other.get_matrix()[k][j] for k in range(other.get_row_count())]) for j in
                  range(other.get_col_count())] for i in range(self.__row)]
+        raise RuntimeError('Число столбцов у первой матрицы не равно числу строк у второй')
 
     def __mul__(self, other):
         if isinstance(other, int):
